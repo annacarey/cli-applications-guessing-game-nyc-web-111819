ded69236816b35cb04e1b7f214ad49052a8bd999
declarative-blog-8005 <anna@annajcarey.com>
2019-11-06 22:23:51 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,9 +2,10 @@
 
 def run_guessing_game
   random_number = rand(1...6).to_s 
-  p random_number
   puts "Guess a number between 1 and 6."
   user_number = gets.chomp
+  p random_number
+  p user_number
   if random_number == user_number
     puts "You guessed the correct number!"
   elsif user_number == "exit"
