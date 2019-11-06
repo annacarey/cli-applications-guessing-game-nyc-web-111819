ff3f7b6c4a59f04131dfbee63dc618ac9a03b9a0
declarative-blog-8005 <anna@annajcarey.com>
2019-11-06 22:20:10 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -10,7 +10,8 @@ def run_guessing_game
   elsif user_number == "exit"
     puts "Goodbye!"
   else 
-    random_numer
+    random_numer.to_i += 1 
+    random_numer.to_s 
     puts "Sorry! The computer guessed #{random_number}."
   end 
 end 
